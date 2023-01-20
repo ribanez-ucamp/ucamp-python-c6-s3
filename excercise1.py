1a9dcae6cfb6f99ae01ce09a00758f427712124d
Rafael <rafael.s.ibanez@gmail.com>
2023-01-19 22:30:00 -0600

    Excercise 1 is finished

---
+++ excercise1.py
@@ -0,0 +1,12 @@
+user_number = input("Por favor digite un número: ")
+user_number_int = int(user_number)
+user_number_divided_by_3 = user_number_int / 3
+user_number_greater_than_2 = user_number_divided_by_3 > 2
+
+# print(type(user_number))
+# print(type(user_number_int))
+# print(user_number)
+# print(user_number_int)
+
+print(user_number_greater_than_2)
+input()
